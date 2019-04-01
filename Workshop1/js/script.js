37d4f7d582fdfd39f518d818bcda992b09c2b0f3
HsuanKang <ting985477@gmail.com>
2019-04-01 17:27:32 +0800

    add delete window

--- Workshop1/js/script.js
+++ Workshop1/js/script.js
@@ -17,32 +17,31 @@ function loadBookData() {
     }
 }
 
-$(function () {
-    loadBookData();
-});
-
-function getIndexById(id) {
-    l = dataSource.length;
-
-    for (var j = 0; j < l; j++) {
-        if (dataSource[j].BookId == id) {
-            return j;
-        }
-    }
-    return null;
-}
 
 $(document).ready(function () {
+    var windowTemplate = kendo.template($("#windowTemplate").html());
+    kendo.culture("zh-CN");
     var dataSource = new kendo.data.DataSource({
         data: bookData,
         pageSize: 20,
+        //transport: {
+        //    destroy: {
+        //        url: crudServiceBaseUrl + "/Products/Destroy",
+        //        dataType: "jsonp"
+        //    }
+        //},
+        //parameterMap: function (options, operation) {
+        //    if (operation !== "read" && options.models) {
+        //        return { models: kendo.stringify(options.models) };
+        //    }
+        //},
         schema: {
             model: {
                 id: "BookId",
                 fields: {
                     BookId: { editable: false, nullable: true },
                     BookName: { validation: { required: true } },
-                    //BookBoughtDate: { type: "date" },
+                    BookBoughtDate: { type: "date" },
                     BookPrice: { type: "number", validation: { required: true, min: 1 } },
                     BookAmount: { type: "number", validation: { required: true, min: 1 } },
                     BookTotal: { type: "number", validation: { required: true, min: 1 } }
@@ -58,7 +57,26 @@ $(document).ready(function () {
         pageable: true,
         sortable: true,
         columns: [
-            { command: ["destroy"], title: "&nbsp;", width: "200px" },
+            {
+                command: [{
+                    name: "delete", text: "刪除", click: function (e) {  //add a click event listener on the delete button
+                        e.preventDefault(); //prevent page scroll reset
+                        var tr = $(e.target).closest("tr"); //get the row for deletion
+                        var data = this.dataItem(tr); //get the row data so it can be referred later
+                        window.content(windowTemplate(data)); //send the row data object to the template and render it
+                        window.center().open();
+
+                        $("#delete_yes").click(function () {
+                            grid.dataSource.remove(data)  //prepare a "destroy" request
+                            grid.dataSource.sync()  //actually send the request (might be ommited if the autoSync option is enabled in the dataSource)
+                            window.close();
+                        })
+                        $("#delete_no").click(function () {
+                            window.close();
+                        })
+                    }
+                }], title: "&nbsp;", width: "100px"
+            },
             { field: "BookId", title: "書籍編號", width: "65px" },
             { field: "BookName", title: "書籍名稱", width: "250px" },
             { field: "BookCategory", title: "書籍種類", width: "100px" },
@@ -68,12 +86,11 @@ $(document).ready(function () {
             { field: "BookPublisher", title: "發行公司", width: "120px" },
             { field: "BookPrice", title: "金額", width: "80px" },
             { field: "BookAmount", title: "數量", width: "80px" },
-            { field: "BookTotal", title: "總計", width: "100px" }
-        ]
-    });
+            { field: "BookTotal", title: "總計", width: "100px" }]
+    }).data("kendoGrid");
 
 
-    $("#add_book").click(function () {
+    $("#add_book").click(function () { //新增書籍視窗
         $("#window").kendoWindow({
             width: 550,
             height: 650,
@@ -85,11 +102,21 @@ $(document).ready(function () {
         win.center().open().center();
     });
 
-    $("#searchBox").on("input", function (e) {
+    $("#searchBox").on("input", function (e) { //搜尋列
         var listBox = $("#book_grid").data("kendoGrid");
-        var sarchString = $(this).val();
+        var searchString = $(this).val();
 
-        listBox.dataSource.filter({ field: "BookName", operator: "contains", value: sarchString },
-                                  { field: "BookAuthor", operator: "contains", value: sarchString });
+        listBox.dataSource.filter({
+            logic: "or",
+            filters: [{ field: "BookName", operator: "contains", value: searchString },
+            { field: "BookAuthor", operator: "contains", value: searchString }]
+        });
     });
+
+    var window = $("#delete_window").kendoWindow({ //刪除確認視窗
+        visible: false,
+        width: "600px",
+        height: "150px",
+    }).data("kendoWindow");
+
 });
