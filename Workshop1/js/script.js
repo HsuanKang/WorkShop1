2bbc267a6e553fdbb55a105bd324ce1a50a7215e
HsuanKang <ting985477@gmail.com>
2019-04-04 03:45:11 +0800

    add require

--- Workshop1/js/script.js
+++ Workshop1/js/script.js
@@ -17,40 +17,14 @@ function loadBookData() {
     }
 }
 
-//function setData() {
-//    var Data = bookData;
-//    localStorage["bookData"] = JSON.stringify(Data);
-//}
-
-
 
 $(document).ready(function () {
-    //if (localStorage["bookData"] == undefined) {
-    //    setData();
-    //}
-    loadBookData();
+    var validator = $("#window").kendoValidator().data("kendoValidator");
     var windowTemplate = kendo.template($("#windowTemplate").html());
     kendo.culture('zh-TW');
     var dataSource = new kendo.data.DataSource({
         data: bookData,
         pageSize: 20,
-        //transport: {
-        //    read: function (options) {
-        //        var localData = JSON.parse(localStorage["bookData"]);
-        //        options.success(localData);
-        //    },
-        //    destroy: function (options) {
-        //        var localData = localStorage["bookData"];
-        //        for (var i = 0; i < localData.length; i++) {
-        //            if (localData[i].BookId === options.data.BookId) {
-        //                localData.splice(i, 1);
-        //                break;
-        //            }
-        //        }
-        //        localStorage["bookData"] = JSON.stringify(localData);
-        //        options.success(localData);
-        //    }
-        //},
         schema: {
             model: {
                 id: "BookId",
@@ -67,6 +41,11 @@ $(document).ready(function () {
         }
     });
 
+    $("#save_book").on("click", function () {
+        if (validator.validate()) {
+            save();
+        }
+    });
 
     var book_grid = $("#book_grid").kendoGrid({
         dataSource: dataSource,
@@ -77,15 +56,15 @@ $(document).ready(function () {
             {
                 command: [{
                     name: "delete", text: "刪除", click: function (e) {
-                        e.preventDefault(); //prevent page scroll reset
-                        var tr = $(e.target).closest("tr"); //get the row for deletion
-                        var data = this.dataItem(tr); //get the row data so it can be referred later
-                        window.content(windowTemplate(data)); //send the row data object to the template and render it
+                        e.preventDefault(); //防止頁面滾動
+                        var tr = $(e.target).closest("tr"); //取得要刪的列
+                        var data = this.dataItem(tr); //列的值
+                        window.content(windowTemplate(data)); //送出資料
                         window.center().open();
 
                         $("#delete_yes").click(function () {
-                            book_grid.dataSource.remove(data)  //prepare a "destroy" request
-                            book_grid.dataSource.sync()  //actually send the request (might be ommited if the autoSync option is enabled in the dataSource)
+                            book_grid.dataSource.remove(data)  //準備刪除
+                            book_grid.dataSource.sync()  //同步送出資料
                             window.close();
                         })
                         $("#delete_no").click(function () {
@@ -104,7 +83,7 @@ $(document).ready(function () {
             { field: "BookPrice", title: "金額", width: "80px", attributes: { "class": "right-align", "data-boo": "foo" }, template: "#=kendo.format('{0:n0}', BookPrice)#" },
             { field: "BookAmount", title: "數量", width: "80px", attributes: { "class": "right-align", "data-boo": "foo" }, template: "#=kendo.format('{0:n0}', BookAmount)#" },
             { field: "BookTotal", title: "總計", width: "100px", attributes: { "class": "right-align", "data-boo": "foo" }, template: "#=kendo.format('{0:n0}', BookTotal)#" }],
-            //editable: "incell"
+        //editable: "incell"
     }).data("kendoGrid");
 
 
